b20a11ebb7a341c06a349635e997ebaf13b73058
fangzhiyue <fangzhiyue@outlook.com>
2018-10-06 18:50:41 +0800

    six submit

--- app/src/main/java/com/example/fang/librarysearch/view/fragment/SettingPageFragment.java
+++ app/src/main/java/com/example/fang/librarysearch/view/fragment/SettingPageFragment.java
@@ -1,23 +1,31 @@
 package com.example.fang.librarysearch.view.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.fang.librarysearch.R;
+import com.example.fang.librarysearch.standard.StandardSet;
+
+import java.io.File;
 
 /**
  * 设置界面的Fragment
  */
 public class SettingPageFragment extends Fragment {
 
+    SharedPreferences sp;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        sp = getContext().getSharedPreferences(StandardSet.historyName, Context.MODE_PRIVATE);
     }
 
     @Nullable
@@ -30,6 +38,9 @@ public class SettingPageFragment extends Fragment {
         return view;
     }
 
+    TextView reset;
+    TextView clearCache;
+
     /**
      * 对里面的子视图进行初始化
      *
@@ -37,6 +48,35 @@ public class SettingPageFragment extends Fragment {
      */
     private void initComponent(ViewGroup view) {
 
+        reset = view.findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //在这里我们清除我们的html网页缓存
+                File file = new File(getContext().getCacheDir(), StandardSet.saveHtmlName);
+                File file1 = new File(getContext().getCacheDir(), StandardSet.reretrievalHtmlData);
+                if (file.exists()) {
+                    file.delete();
+                }
+                if (file1.exists()) {
+                    file1.delete();
+                }
+            }
+        });
+
+        clearCache = view.findViewById(R.id.clearCache);
+        clearCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //在这里我们清除  配置文件
+                if (sp != null) {
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.clear();
+                    editor.commit();
+
+                }
+            }
+        });
 
     }
 }
